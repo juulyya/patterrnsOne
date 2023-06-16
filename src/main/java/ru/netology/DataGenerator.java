02d890ed5be1c964ca3930edf2a79c9075d8319b
juulyya <yuliya.storozhenko.1994@mail.ru>
2023-06-16 19:35:29 +0400

    Initail commit

---
+++ src/main/java/ru/netology/DataGenerator.java
@@ -0,0 +1,53 @@
+package ru.netology;
+import com.github.javafaker.Faker;
+import lombok.Value;
+import lombok.val;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));;
+    }
+
+    public static String generateCity(String locale) {
+        var cities = new String[]{"Самара", "Москва", "Тольятти", "Казань", "Волгоград", "Ростов-на-Дону", "Краснодар", "Санкт-Петербург", "Владивосток",
+                "Сызрань", "Ульяновск", "Оренбург", "Уфа", "Пермь"};
+
+        return cities[new Random().nextInt(cities.length)];
+    }
+
+    public static String generateName(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
+
+    public static String generatePhone(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
+            // generateName(locale), generatePhone(locale)
+            return user;
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
